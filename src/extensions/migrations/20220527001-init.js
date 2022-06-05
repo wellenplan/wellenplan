1cf634d572761efe45cccb07cbbe2bfd3c6ab310
Lucas Bickel <hairmare@purplehaze.ch>
2022-06-05 15:25:04 +0200

    fix: make table creation work with cockroachdb

--- src/extensions/migrations/20220527001-init.js
+++ src/extensions/migrations/20220527001-init.js
@@ -3,7 +3,7 @@ module.exports = {
   async up(knex) {
     await knex.transaction(async (transaction) => {
       await transaction.schema.createTable("wellenplan_shows", (table) => {
-        table.uuid("id").primary();
+        table.uuid("id").notNullable().primary();
         table.string("status").notNullable().defaultTo("draft");
         table.uuid("user_created");
         table.foreign("user_created").references("directus_users.id");
@@ -16,7 +16,7 @@ module.exports = {
       await transaction.schema.createTable(
         "wellenplan_show_episodes",
         (table) => {
-          table.uuid("id").primary();
+          table.uuid("id").notNullable().primary();
           table.string("status").notNullable().defaultTo("draft");
           table.integer("sort");
           table.uuid("user_created");
@@ -33,7 +33,7 @@ module.exports = {
         }
       );
       await transaction.schema.createTable("wellenplan_hourclocks", (table) => {
-        table.uuid("id").primary();
+        table.uuid("id").notNullable().primary();
         table.string("status").notNullable().defaultTo("draft");
         table.integer("sort");
         table.uuid("user_created");
